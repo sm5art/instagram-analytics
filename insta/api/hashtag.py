f5c5bd283afa5cf837349e1dd0e5eb2fbe2ff5d7
Artur K <art@network.ru>
2017-09-23 14:13:37 -0700

    good shit right heree

--- insta/api/hashtag.py
+++ insta/api/hashtag.py
@@ -6,4 +6,19 @@ class HashTag(BaseAPI):
         super(HashTag, self).__init__("GET")
         uri = "tags/search"
         json = self.request(uri, {"q": hashtag})
-        print(json)
+        self.data = json
+
+class TagInfo(BaseAPI):
+    def __init__(self, hashtag):
+        super(TagInfo, self).__init__("GET")
+        uri = "tags/%s" % hashtag
+        json = self.request(uri, {})
+        self.data = json
+
+
+class RecentTagPosts(BaseAPI):
+    def __init__(self, hashtag):
+        super(RecentTagPosts, self).__init__("GET")
+        uri = "tags/%s/media/recent" % hashtag
+        json = self.request(uri, {})
+        self.data = json
